28ddb1d61f1c868ce4c0940eee3b29f7d08c72be
Ryan Liang <zhicheng.liang@oracle.com>
2014-01-11 18:48:33 -0500

    add value limit as input varaiable

--- lib/oos.rb
+++ lib/oos.rb
@@ -2,7 +2,7 @@ module OOS
   COMMENT_LINE_NUM = 0
   HEADER_LINE_NUM  = 1
   OTU_PREFIX = 'denovo'
-  VALUE_LIMIT = 500
+  DEFAULT_VALUE_LIMIT = 500
 
   def main
     if ARGV.count < 2
@@ -11,13 +11,16 @@ module OOS
     end
 
     oos_dir, out_file = ARGV[0], ARGV[1]
-    raw_flag = true if ARGV.length == 3 
+
+    raw_flag = ARGV[-1] == '-r' ? true : false
+    value_limit = (ARGV.count > 2 && ARGV[2] =~ /^[0-9]+$/) ? ARGV[2].to_i : DEFAULT_VALUE_LIMIT
+
     ARGV.clear
     
     result = process(oos_dir)
     master_summary, comment, header = result[:summary], result[:comment], result[:header]
 
-    output_result(comment, header, master_summary, out_file, raw_flag)
+    output_result(comment, header, master_summary, out_file, raw_flag, value_limit)
   end # def
 
   def process(oos_dir)
@@ -45,8 +48,9 @@ module OOS
 
   def print_usage
     puts "Incorrect number of parameters"
-    puts "Usage:   oos.rb 'dir_to_oos_files' 'output_file_path"
+    puts "Usage:   oos.rb 'dir_to_oos_files' 'output_file_path [limit -r]"
     puts "Example: oos.rb 'C:\\temp\\ivy\\shared\\trialforsummarytable' 'c:\\temp\\ivy\\shared\\output.txt'"
+    puts "Example: oos.rb 'C:\\temp\\ivy\\shared\\trialforsummarytable' 'c:\\temp\\ivy\\shared\\output.txt' 400 -r"
   end # def
 
   def output_result(*args)
@@ -55,6 +59,7 @@ module OOS
     summary   = args[2]
     file_path = args[3]
     raw_flag  = args[4]
+    value_limit = args[5]
 
     File.open(file_path, 'w') do |file|
       file.write(comment)
@@ -63,8 +68,8 @@ module OOS
         if raw_flag
           file.write(otu_occur.to_s)
         else
-          unless otu_occur.all_value_less_than?(VALUE_LIMIT)
-            otu_occur.replace_value_if_less_than_limit(VALUE_LIMIT, 0)
+          unless otu_occur.all_value_less_than?(value_limit)
+            otu_occur.replace_value_if_less_than_limit(value_limit, 0)
             file.write(otu_occur.to_s)
           end
         end
